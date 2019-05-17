b3b047bcbb4530244852cb31aa37a5fe30e31d0e
quanyousheng <andygo520@163.com>
2019-05-17 20:34:54 +0800

    登录页增加勾选协议

--- app/src/main/java/com/whpe/qrcode/shandong_jining/activity/ActivityLogin.java
+++ app/src/main/java/com/whpe/qrcode/shandong_jining/activity/ActivityLogin.java
@@ -1,8 +1,11 @@
 package com.whpe.qrcode.shandong_jining.activity;
 
+import android.content.Context;
+import android.support.constraint.Group;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,6 +13,7 @@ import android.widget.TextView;
 import com.whpe.qrcode.shandong_jining.R;
 import com.whpe.qrcode.shandong_jining.bigtools.GlobalConfig;
 import com.whpe.qrcode.shandong_jining.bigtools.IDUtils;
+import com.whpe.qrcode.shandong_jining.bigtools.SPHelper;
 import com.whpe.qrcode.shandong_jining.bigtools.ToastUtils;
 import com.whpe.qrcode.shandong_jining.net.JsonComomUtils;
 import com.whpe.qrcode.shandong_jining.net.action.GetSmsAction;
@@ -34,6 +38,8 @@ public class ActivityLogin extends NormalTitleActivity implements View.OnClickLi
     private ImageView iv_delete_phone;
     private Button btn_login;
     private String st_phone;
+    private CheckBox cb_select;
+    private TextView tv_rule;
 
     @Override
     protected void afterLayout() {
@@ -52,58 +58,67 @@ public class ActivityLogin extends NormalTitleActivity implements View.OnClickLi
 
     @Override
     protected void onCreateInitView() {
-        et_phone.addTextChangedListener(new LoginPhoneEditextChangeListner(this,btn_login,iv_delete_phone));
+        et_phone.addTextChangedListener(new LoginPhoneEditextChangeListner(this, btn_login, iv_delete_phone));
         iv_delete_phone.setOnClickListener(this);
         btn_login.setOnClickListener(this);
         btn_send.setOnClickListener(this);
+        tv_rule.setOnClickListener(this);
     }
 
     @Override
     protected void onCreatebindView() {
         btn_send = findViewById(R.id.btn_send);
-        et_phone = (EditText)findViewById(R.id.et_phone);
-        et_verify = (EditText)findViewById(R.id.et_verify);
-        iv_delete_phone = (ImageView)findViewById(R.id.iv_delete_phone);
-        btn_login = (Button)findViewById(R.id.btn_login);
+        et_phone = (EditText) findViewById(R.id.et_phone);
+        et_verify = (EditText) findViewById(R.id.et_verify);
+        iv_delete_phone = (ImageView) findViewById(R.id.iv_delete_phone);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        cb_select = findViewById(R.id.cb_select);
+        tv_rule = findViewById(R.id.tv_rule);
     }
 
     @Override
     public void onClick(View view) {
-        int id=view.getId();
+        int id = view.getId();
         st_phone = et_phone.getText().toString();
-        String st_vertify= et_verify.getText().toString();
-        if(id==R.id.iv_delete_phone){
+        String st_vertify = et_verify.getText().toString();
+        if (id == R.id.iv_delete_phone) {
             et_phone.setText("");
-        }else if(id==R.id.btn_login){
-            if(TextUtils.isEmpty(st_phone)||TextUtils.isEmpty(st_vertify)){
-                ToastUtils.showToast(this,getString(R.string.login_promt_notinputinfo));
+        } else if (id == R.id.btn_login) {
+            if (TextUtils.isEmpty(st_phone) || TextUtils.isEmpty(st_vertify)) {
+                ToastUtils.showToast(this, getString(R.string.login_promt_notinputinfo));
                 return;
             }
-            if(!IDUtils.isMobileNO(st_phone)){
-                ToastUtils.showToast(this,getString(R.string.login_promt_phoneform_error));
+            if (!IDUtils.isMobileNO(st_phone)) {
+                ToastUtils.showToast(this, getString(R.string.login_promt_phoneform_error));
                 return;
             }
-            if(st_vertify.length()!=6){
-                ToastUtils.showToast(this,getString(R.string.login_promt_vertifyform_error));
+            if (st_vertify.length() != 6) {
+                ToastUtils.showToast(this, getString(R.string.login_promt_vertifyform_error));
+                return;
+            }
+            if (!cb_select.isChecked()) {
+                ToastUtils.showToast(this, getString(R.string.login_select_checkbox));
                 return;
             }
             showProgress();
-            LoginAction loginAction=new LoginAction(this,this);
-            loginAction.sendAction(st_phone,st_vertify);
-        }else if(id==R.id.btn_send){
-            if(TextUtils.isEmpty(st_phone)){
-                ToastUtils.showToast(this,getString(R.string.login_promt_notinputphone));
+            LoginAction loginAction = new LoginAction(this, this);
+            loginAction.sendAction(st_phone, st_vertify);
+        } else if (id == R.id.btn_send) {
+            if (TextUtils.isEmpty(st_phone)) {
+                ToastUtils.showToast(this, getString(R.string.login_promt_notinputphone));
                 return;
             }
-            if(!IDUtils.isMobileNO(st_phone)){
-                ToastUtils.showToast(this,getString(R.string.login_promt_phoneform_error));
+            if (!IDUtils.isMobileNO(st_phone)) {
+                ToastUtils.showToast(this, getString(R.string.login_promt_phoneform_error));
                 return;
             }
             CountDownTimerUtils countDownTimerUtils = new CountDownTimerUtils(btn_send, 60000, 1000);
             countDownTimerUtils.start();
             showProgress();
-            GetSmsAction getSmsAction=new GetSmsAction(this,this);
+            GetSmsAction getSmsAction = new GetSmsAction(this, this);
             getSmsAction.sendAction(st_phone);
+        } else if (id == R.id.tv_rule) {
+            transAty(ActivityUsehelp.class);
         }
     }
 
@@ -111,13 +126,13 @@ public class ActivityLogin extends NormalTitleActivity implements View.OnClickLi
     public void onQuerySmsSucces(ArrayList<String> getinfo) {
         dissmissProgress();
         try {
-            if(getinfo.get(0).equals(GlobalConfig.RESCODE_SUCCESS)){
-                ToastUtils.showToast(this,getString(R.string.login_sendsms_true));
-            }else {
-                ToastUtils.showToast(this,getinfo.get(1));
-                loginCheckAllUpadate(getinfo.get(0),getinfo);
+            if (getinfo.get(0).equals(GlobalConfig.RESCODE_SUCCESS)) {
+                ToastUtils.showToast(this, getString(R.string.login_sendsms_true));
+            } else {
+                ToastUtils.showToast(this, getinfo.get(1));
+                loginCheckAllUpadate(getinfo.get(0), getinfo);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             showExceptionAlertDialog();
         }
     }
@@ -125,31 +140,31 @@ public class ActivityLogin extends NormalTitleActivity implements View.OnClickLi
     @Override
     public void onQuerySmsFaild(String resmsg) {
         dissmissProgress();
-        ToastUtils.showToast(this,getString(R.string.login_sendsms_false));
+        ToastUtils.showToast(this, getString(R.string.login_sendsms_false));
     }
 
     @Override
     public void onLoginSucces(ArrayList<String> getinfo) {
         dissmissProgress();
         try {
-            String rescode=getinfo.get(0);
-            if(rescode.equals(GlobalConfig.RESCODE_SUCCESS)){
-                LoginBean loginBean=new LoginBean();
-                loginBean= (LoginBean) JsonComomUtils.parseAllInfo(getinfo.get(2),loginBean);
+            String rescode = getinfo.get(0);
+            if (rescode.equals(GlobalConfig.RESCODE_SUCCESS)) {
+                LoginBean loginBean = new LoginBean();
+                loginBean = (LoginBean) JsonComomUtils.parseAllInfo(getinfo.get(2), loginBean);
                 sharePreferenceLogin.saveLoginStatus(true);
                 sharePreferenceLogin.saveLoginPhone(st_phone);
                 sharePreferenceLogin.saveUid(loginBean.getUid());
                 sharePreferenceLogin.saveToken(loginBean.getToken());
                 finish();
-            }else {
-                if(TextUtils.isEmpty(getinfo.get(1))){
-                    ToastUtils.showToast(this,getString(R.string.login_login_false));
-                }else {
-                    ToastUtils.showToast(this,getinfo.get(1));
-                    loginCheckAllUpadate(rescode,getinfo);
+            } else {
+                if (TextUtils.isEmpty(getinfo.get(1))) {
+                    ToastUtils.showToast(this, getString(R.string.login_login_false));
+                } else {
+                    ToastUtils.showToast(this, getinfo.get(1));
+                    loginCheckAllUpadate(rescode, getinfo);
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             showExceptionAlertDialog();
         }
     }
@@ -157,7 +172,7 @@ public class ActivityLogin extends NormalTitleActivity implements View.OnClickLi
     @Override
     public void onLoginFaild(String resmsg) {
         dissmissProgress();
-        ToastUtils.showToast(this,getString(R.string.login_login_false));
+        ToastUtils.showToast(this, getString(R.string.login_login_false));
     }
 
 }
--- app/src/main/java/com/whpe/qrcode/shandong_jining/activity/ActivityUsehelp.java
+++ app/src/main/java/com/whpe/qrcode/shandong_jining/activity/ActivityUsehelp.java
@@ -1,14 +1,24 @@
 package com.whpe.qrcode.shandong_jining.activity;
 
 
+import android.content.Context;
+import android.net.http.SslError;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import com.whpe.qrcode.shandong_jining.R;
 import com.whpe.qrcode.shandong_jining.parent.NormalTitleActivity;
+import com.whpe.qrcode.shandong_jining.view.ProgressWebView;
 
 /**
  * Created by yang on 2018/10/3.
  */
 
 public class ActivityUsehelp extends NormalTitleActivity {
+    private ProgressWebView wv;
+
     @Override
     protected void afterLayout() {
         super.afterLayout();
@@ -28,11 +38,50 @@ public class ActivityUsehelp extends NormalTitleActivity {
     @Override
     protected void onCreateInitView() {
         super.onCreateInitView();
-        setTitle(getString(R.string.usehelp_title));
+//        setTitle(getString(R.string.usehelp_title));
+        initWebview();
+
     }
 
     @Override
     protected void onCreatebindView() {
         super.onCreatebindView();
+        wv = findViewById(R.id.wv);
+    }
+
+    private void initWebview() {
+        WebSettings settings = wv.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        settings.setDefaultTextEncodingName("UTF-8");//设置网页默认编码
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        settings.setLoadWithOverviewMode(true);
+        settings.setSupportZoom(true);          //允许缩放
+        settings.setBuiltInZoomControls(true);  //原网页基础上缩放
+        settings.setDisplayZoomControls(false);
+        settings.setUseWideViewPort(true);      //任意比例缩放
+
+
+        settings.setAppCacheEnabled(true);
+        String appCaceDir = this.getApplicationContext().getDir("cache", Context.MODE_PRIVATE).getPath();
+        settings.setAppCachePath(appCaceDir);
+        // 格式规定为:file:////android_asset/文件名.html
+        wv.loadUrl("file:////android_asset/rule.html");
+        wv.setWebViewClient(new WebViewClient() {
+            //2017 10-19webview无法读取页面由于https因此修改
+            @Override
+            public void onReceivedSslError(WebView view,
+                                           SslErrorHandler handler, SslError error) {
+                // TODO Auto-generated method stub
+                // handler.cancel();// Android默认的处理方式
+                handler.proceed();// 接受所有网站的证书
+                // handleMessage(Message msg);// 进行其他处理
+            }
+
+        });
     }
 }
