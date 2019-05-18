b9e22d8f14d30c8bd339c3b1b8bc495897c287a4
quanyousheng <andygo520@163.com>
2019-05-18 14:58:36 +0800

    云充金额修改

--- app/src/main/java/com/whpe/qrcode/shandong_jining/fragment/cloudrechargecard/FrgCloudRechargeCardTopay.java
+++ app/src/main/java/com/whpe/qrcode/shandong_jining/fragment/cloudrechargecard/FrgCloudRechargeCardTopay.java
@@ -47,8 +47,8 @@ public class FrgCloudRechargeCardTopay extends Fragment implements PayUnifyActio
     private Button btn_submit;
     private GridView gv_paytype;
     private RechargeCardPaytypeLvAdapter rechargeCardPaytypeLvAdapter;
-    private ArrayList<PaytypeRechargeCardBean> paytypeRechargeCardBeans=new ArrayList<>();
-    private LoadQrcodeParamBean loadQrcodeParamBean=new LoadQrcodeParamBean();
+    private ArrayList<PaytypeRechargeCardBean> paytypeRechargeCardBeans = new ArrayList<>();
+    private LoadQrcodeParamBean loadQrcodeParamBean = new LoadQrcodeParamBean();
     private EditText et_cardnoagain;
     private EditText et_cardno;
     private String cardno;
@@ -57,27 +57,27 @@ public class FrgCloudRechargeCardTopay extends Fragment implements PayUnifyActio
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=LayoutInflater.from(getContext()).inflate(R.layout.frg_cloudrechargecard_topay,container,false);
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.frg_cloudrechargecard_topay, container, false);
         return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        content=view;
-        context=getContext();
-        activity= (ActivityCloudRechargeCard) getActivity();
-        loadQrcodeParamBean=activity.loadQrcodeParamBean;
+        content = view;
+        context = getContext();
+        activity = (ActivityCloudRechargeCard) getActivity();
+        loadQrcodeParamBean = activity.loadQrcodeParamBean;
         bindView();
         initView();
     }
 
     private void bindView() {
-        gv_money = (GridView)content.findViewById(R.id.gvmoney);
-        btn_submit = (Button)content.findViewById(R.id.btn_submit);
+        gv_money = (GridView) content.findViewById(R.id.gvmoney);
+        btn_submit = (Button) content.findViewById(R.id.btn_submit);
         gv_paytype = content.findViewById(R.id.gv_paytype);
-        et_cardnoagain = (EditText)content.findViewById(R.id.et_card_again);
-        et_cardno = (EditText)content.findViewById(R.id.et_card);
+        et_cardnoagain = (EditText) content.findViewById(R.id.et_card_again);
+        et_cardno = (EditText) content.findViewById(R.id.et_card);
     }
 
     private void initView() {
@@ -92,20 +92,20 @@ public class FrgCloudRechargeCardTopay extends Fragment implements PayUnifyActio
             public void onClick(View view) {
                 cardno = et_cardno.getText().toString();
                 cardnoagain = et_cardnoagain.getText().toString();
-                if(TextUtils.isEmpty(cardno)||TextUtils.isEmpty(cardnoagain)){
-                    ToastUtils.showToast(context,getString(R.string.cloudrecharge_toast_pleaseinput));
+                if (TextUtils.isEmpty(cardno) || TextUtils.isEmpty(cardnoagain)) {
+                    ToastUtils.showToast(context, getString(R.string.cloudrecharge_toast_pleaseinput));
                     return;
                 }
-                if(!cardno.equals(cardnoagain)){
-                    ToastUtils.showToast(context,getString(R.string.cloudrecharge_toast_pleasetwiceequals));
+                if (!cardno.equals(cardnoagain)) {
+                    ToastUtils.showToast(context, getString(R.string.cloudrecharge_toast_pleasetwiceequals));
                     return;
                 }
-                if(cardno.length()<16){
-                    ToastUtils.showToast(context,getString(R.string.cloudrecharge_toast_cardnoerror));
+                if (cardno.length() < 16) {
+                    ToastUtils.showToast(context, getString(R.string.cloudrecharge_toast_cardnoerror));
                     return;
                 }
-                if(TextUtils.isEmpty(rechargemoney)){
-                    ToastUtils.showToast(context,getString(R.string.cloudrecharge_toast_pleaseselectmoney));
+                if (TextUtils.isEmpty(rechargemoney)) {
+                    ToastUtils.showToast(context, getString(R.string.cloudrecharge_toast_pleaseselectmoney));
                     return;
                 }
                 requestForPayUnity();
@@ -116,7 +116,7 @@ public class FrgCloudRechargeCardTopay extends Fragment implements PayUnifyActio
     //填充支付方式选择目录
     private void initListViewPaytype() {
         initPaytype();
-        rechargeCardPaytypeLvAdapter = new RechargeCardPaytypeLvAdapter(context,paytypeRechargeCardBeans);
+        rechargeCardPaytypeLvAdapter = new RechargeCardPaytypeLvAdapter(context, paytypeRechargeCardBeans);
         gv_paytype.setAdapter(rechargeCardPaytypeLvAdapter);
         gv_paytype.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -129,10 +129,10 @@ public class FrgCloudRechargeCardTopay extends Fragment implements PayUnifyActio
 
     //填充支付方式
     private void initPaytype() {
-        List<LoadQrcodeParamBean.CityQrParamConfigBean.PayWayBean> payWayBeans=loadQrcodeParamBean.getCityQrParamConfig().getPayWay();
-        for(int i=0;i<payWayBeans.size();i++){
-            if(payWayBeans.get(i).getPayWayType().equals(GlobalConfig.LOADPARAM_QROPAYTYPE_PREPAY)){
-                PaytypeRechargeCardBean paytypeRechargeCardBean=new PaytypeRechargeCardBean();
+        List<LoadQrcodeParamBean.CityQrParamConfigBean.PayWayBean> payWayBeans = loadQrcodeParamBean.getCityQrParamConfig().getPayWay();
+        for (int i = 0; i < payWayBeans.size(); i++) {
+            if (payWayBeans.get(i).getPayWayType().equals(GlobalConfig.LOADPARAM_QROPAYTYPE_PREPAY)) {
+                PaytypeRechargeCardBean paytypeRechargeCardBean = new PaytypeRechargeCardBean();
                 paytypeRechargeCardBean.setPayWayCode(payWayBeans.get(i).getPayWayCode());
                 paytypeRechargeCardBean.setPayWayName(payWayBeans.get(i).getPayWayName());
                 paytypeRechargeCardBeans.add(paytypeRechargeCardBean);
@@ -155,55 +155,54 @@ public class FrgCloudRechargeCardTopay extends Fragment implements PayUnifyActio
     }
 
     //填充实际发送金额数据
-    private void initRechargeMoney(int i){
-        String[] money_recharges=getResources().getStringArray(R.array.rechargecard_payunity_money);
-        rechargemoney=money_recharges[i];
+    private void initRechargeMoney(int i) {
+        String[] money_recharges = getResources().getStringArray(R.array.rechargecard_payunity_money);
+        rechargemoney = money_recharges[i];
     }
 
     //请求下单
     private void requestForPayUnity() {
         activity.showCloudRechargeCardProgress();
-        PayUnifyAction payUnifyAction=new PayUnifyAction(activity,this);
-        Integer int_rechargemoney=Integer.parseInt(rechargemoney);
-        payUnifyAction.sendAction(int_rechargemoney,GlobalConfig.PAYUNITY_TYPE_BUSCARDAMOUNT_TOPAY,cardno.substring(cardno.length()-16,cardno.length()),rechargeCardPaytypeLvAdapter.getPaytypeCode(),activity.sharePreferenceLogin.getLoginPhone(),GlobalConfig.PAYUNITY_TYPE_BUSCARD_CARDAREA_CPUNORMAL,GlobalConfig.PAYUNITY_TYPE_BUSCARD_READWAY_CLOUD,GlobalConfig.PAYUNITY_TYPE_BUSCARD_BALANCE_NORMAL);
+        PayUnifyAction payUnifyAction = new PayUnifyAction(activity, this);
+        Integer int_rechargemoney = Integer.parseInt(rechargemoney);
+        payUnifyAction.sendAction(int_rechargemoney, GlobalConfig.PAYUNITY_TYPE_BUSCARDAMOUNT_TOPAY, cardno.substring(cardno.length() - 16, cardno.length()), rechargeCardPaytypeLvAdapter.getPaytypeCode(), activity.sharePreferenceLogin.getLoginPhone(), GlobalConfig.PAYUNITY_TYPE_BUSCARD_CARDAREA_CPUNORMAL, GlobalConfig.PAYUNITY_TYPE_BUSCARD_READWAY_CLOUD, GlobalConfig.PAYUNITY_TYPE_BUSCARD_BALANCE_NORMAL);
     }
 
     //解析数据
     private void solveGWPayCallback(ArrayList<String> getinfo) {
         try {
-            String rescode=getinfo.get(0);
-            if(rescode.equals(GlobalConfig.RESCODE_SUCCESS)){
-                activity.havepay=getString(R.string.cloudrecharge_havepay_yes);
+            String rescode = getinfo.get(0);
+            if (rescode.equals(GlobalConfig.RESCODE_SUCCESS)) {
+                activity.havepay = getString(R.string.cloudrecharge_havepay_yes);
                 startToPay(getinfo);
-            }else{
-                activity.checkAllUpadate(rescode,getinfo);
+            } else {
+                activity.checkAllUpadate(rescode, getinfo);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             activity.showExceptionAlertDialog();
         }
     }
 
 
-
     private void startToPay(ArrayList<String> getinfo) {
-        if(rechargeCardPaytypeLvAdapter.getPaytypeCode().equals(GlobalConfig.LOADPARAM_QROPAYTYPE_PREPAYUNION)){
-            UnionBean unionBean=new UnionBean();
-            unionBean= (UnionBean) JsonComomUtils.parseAllInfo(getinfo.get(2),unionBean);
-            activity.merchantOderNo=unionBean.getMerchantOderNo();
-            PayUtils.unionPay(activity,unionBean.getPayParam().getTn());
+        if (rechargeCardPaytypeLvAdapter.getPaytypeCode().equals(GlobalConfig.LOADPARAM_QROPAYTYPE_PREPAYUNION)) {
+            UnionBean unionBean = new UnionBean();
+            unionBean = (UnionBean) JsonComomUtils.parseAllInfo(getinfo.get(2), unionBean);
+            activity.merchantOderNo = unionBean.getMerchantOderNo();
+            PayUtils.unionPay(activity, unionBean.getPayParam().getTn());
             return;
         }
-        if(rechargeCardPaytypeLvAdapter.getPaytypeCode().equals(GlobalConfig.LOADPARAM_QROPAYTYPE_PREPAYALIPAY)){
-            AlipayBean alipayBean=new AlipayBean();
-            alipayBean= (AlipayBean) JsonComomUtils.parseAllInfo(getinfo.get(2),alipayBean);
-            activity.merchantOderNo=alipayBean.getMerchantOderNo();
-            PayUtils.aliPay(activity,alipayBean.getPayParam().getOrderStr(),activity.aliHandler);
+        if (rechargeCardPaytypeLvAdapter.getPaytypeCode().equals(GlobalConfig.LOADPARAM_QROPAYTYPE_PREPAYALIPAY)) {
+            AlipayBean alipayBean = new AlipayBean();
+            alipayBean = (AlipayBean) JsonComomUtils.parseAllInfo(getinfo.get(2), alipayBean);
+            activity.merchantOderNo = alipayBean.getMerchantOderNo();
+            PayUtils.aliPay(activity, alipayBean.getPayParam().getOrderStr(), activity.aliHandler);
             return;
         }
-        if(rechargeCardPaytypeLvAdapter.getPaytypeCode().equals(GlobalConfig.LOADPARAM_QROPAYTYPE_PREPAYWEICHAT)){
-            WeichatBean weichatBean=new WeichatBean();
-            weichatBean= (WeichatBean) JsonComomUtils.parseAllInfo(getinfo.get(2),weichatBean);
-            PayUtils.weichatPay(activity,weichatBean);
+        if (rechargeCardPaytypeLvAdapter.getPaytypeCode().equals(GlobalConfig.LOADPARAM_QROPAYTYPE_PREPAYWEICHAT)) {
+            WeichatBean weichatBean = new WeichatBean();
+            weichatBean = (WeichatBean) JsonComomUtils.parseAllInfo(getinfo.get(2), weichatBean);
+            PayUtils.weichatPay(activity, weichatBean);
             return;
         }
         activity.showExceptionAlertDialog(getString(R.string.app_function_notopen));
